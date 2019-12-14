1c1771225ebe31490b360324af813ac8567bdf74
sourav singh <40472249+sourav9124@users.noreply.github.com>
2019-12-14 12:30:48 +0530

    Solved Successfully !!!

---
+++ Day 0 code.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    string s;
+    getline(cin,s);
+    
+    cout<<"Hello, World. "<<endl;
+    cout<<s<<endl;
+
+}
+
+
+
